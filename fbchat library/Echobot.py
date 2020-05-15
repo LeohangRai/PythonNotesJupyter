322b8b90b803923151694ce97379e3fda1c20e98
LeohangRai <58897435+LeohangRai@users.noreply.github.com>
2020-05-16 01:42:12 +0545

    ...

--- fbchat library/Echobot.py
+++ fbchat library/Echobot.py
@@ -5,7 +5,62 @@ from fbchat import log, Client
 username = "raileohang@gmail.com"
 password = "$@higgledy_crucifixation@"
 # Subclass fbchat.Client and override required methods
-class EchoBot(Client):
+
+def memefy(sentence):
+        #Splitting the sentence into words    
+        words = sentence.split()    
+        new_words = []
+        
+        for word in words:
+            
+            each_word_letter_list = []
+            for letter in word:
+                each_word_letter_list.append(letter)
+            
+            
+            each_word_replaced_letters = []
+            for letter in each_word_letter_list:            
+                if letter != 'a' and letter != 's' and letter != 'h' and letter != 'o':
+                    letter = letter
+                elif letter == 'a':
+                    letter = '@'
+                elif letter == 's':
+                    letter = '$'
+                elif letter == 'h':
+                    letter = '#'
+                elif letter == 'o':
+                    letter = '0'
+                each_word_replaced_letters.append(letter)
+                
+            new_word = "".join(each_word_replaced_letters)
+            new_words.append(new_word)
+        
+        try:
+            #Capitalizing the 4th word of the sentence
+            new_words[3] = new_words[3].upper()
+            
+            #Memefying the sixth word of the sentence
+            sixth_word_letters = [i for i in new_words[5]]
+            index = 0
+            mem_sixth= []
+            
+            while index < len(sixth_word_letters):
+                if index % 2 == 0:
+                    mem_sixth.append(sixth_word_letters[index])
+                else:
+                    mem_sixth.append(sixth_word_letters[index].upper())
+                index += 1
+            sixth_word = "".join(mem_sixth)
+            new_words[5] = sixth_word
+            
+        except:
+            pass
+            
+        memefied = " ".join(new_words)
+        return memefied
+
+class EchoBot(Client):   
+                
     def onMessage(self, author_id, message_object, thread_id, thread_type, **kwargs):
         self.markAsDelivered(thread_id, message_object.uid)
         self.markAsRead(thread_id)
